0f7a1eb09e155f81c3021d32581cc9ba1b1c59e2
ReuS <kahlumba@protonmail.com>
2025-05-07 08:40:01 +1100

    Version del

--- plutorcon/__init__.py
+++ plutorcon/__init__.py
@@ -2,4 +2,3 @@ from .client import PlutoniumRCON
 from .models import Player, Commands, ServerInfo, Colors, ServerStatus
 
 __all__ = ["PlutoniumRCON", "Player", "Commands", "ServerInfo", "Colors", "ServerStatus"]
-__version__ = "0.1.0"
