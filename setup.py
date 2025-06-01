d55f39c4050e6e3d2c6f568e6b8d2a32a6419c1f
ReuS <kahlumba@protonmail.com>
2025-06-02 04:57:24 +1100

    Added projet url in `setup.py`

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='plutorcon',
     author='ReuS',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     description='Python RCON client for Plutonium',
+    url="https://github.com/reusteur73/plutorcon",  # lien principal
+    project_urls={
+        "Documentation": "https://github.com/reusteur73/plutorcon#readme",
+        "Source Code": "https://github.com/reusteur73/plutorcon",
+        "Bug Tracker": "https://github.com/reusteur73/plutorcon/issues",
+    },
 )
