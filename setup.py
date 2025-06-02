c145b2b947964d5daccf6d45f0f1bca47609adc8
ReuS <kahlumba@protonmail.com>
2025-06-03 08:34:10 +1100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='plutorcon',
     author='ReuS',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown',
