4111efb0133b48f922531089fc2c8e7d6f329263
ReuS <kahlumba@protonmail.com>
2025-06-03 08:32:48 +1100

    Added `long_description` to `setup.py` for pipy

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='plutorcon',
     author='ReuS',
     version='1.0.2',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     description='Python RCON client for Plutonium',
-    url="https://github.com/reusteur73/plutorcon",  # lien principal
+    url="https://github.com/reusteur73/plutorcon",
     project_urls={
         "Documentation": "https://github.com/reusteur73/plutorcon#readme",
         "Source Code": "https://github.com/reusteur73/plutorcon",
